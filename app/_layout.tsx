904617b46978b8fef3a88825d0673029ff7b84e9
Gaurav-Gali <gauravgali.dev@gmail.com>
2024-10-25 21:43:06 +0530

    setup commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,37 +1,25 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import { Stack } from "expo-router";
+// Setting Up Custom Fonts
+import { useFonts } from "expo-font";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
+import "@/global.css";
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
+    //Defining fonts
+    useFonts({
+        // Playfair Display
+        "playfair": require("../assets/fonts/playfair_display/PlayfairDisplay-Regular.ttf"),
+        "playfair-medium": require("../assets/fonts/playfair_display/PlayfairDisplay-Medium.ttf"),
+        "playfair-bold": require("../assets/fonts/playfair_display/PlayfairDisplay-Bold.ttf"),
 
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-    </ThemeProvider>
-  );
+        // Roboto
+        "roboto": require("../assets/fonts/roboto/Roboto-Regular.ttf"),
+        "roboto-medium": require("../assets/fonts/roboto/Roboto-Medium.ttf"),
+        "roboto-bold": require("../assets/fonts/roboto/Roboto-Bold.ttf"),
+    });
+    return (
+        <Stack>
+            <Stack.Screen name="index" />
+        </Stack>
+    );
 }
